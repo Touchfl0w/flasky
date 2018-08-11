28e65d93c70195ec6720fec19da62491a51111b1
openlab <fu_zhipeng@163.com>
2018-08-11 10:33:11 +0800

    run with flask-script

--- flasky.py
+++ flasky.py
@@ -1,8 +1,18 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_script import Manager
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    current_time = datetime.utcnow()
+    return render_template("index.html", current_time=current_time)
 
 @app.route('/<name>')
 def hello_world(name):
@@ -17,4 +27,5 @@ def internal_server_error(e):
     return render_template("500.html"),500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #用manager接管app启动后，不要传任何参数
+    manager.run()
