c591f5df9135560d4e71e4a08ab00e450b341594
openlab <fu_zhipeng@163.com>
2018-08-13 07:32:57 +0800

    第五章完结

--- flasky.py
+++ flasky.py
@@ -1,19 +1,65 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from forms import NameForm
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hello flasky'
+base_dir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+ os.path.join(base_dir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
-@app.route('/')
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String,unique=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db,User=User,Role=Role)
+
+@app.route('/',methods=['get','post'])
 def index():
-    current_time = datetime.utcnow()
-    return render_template("index.html", current_time=current_time)
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if not user:
+            new_user = User(username = form.name.data)
+            db.session.add(new_user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
+    return render_template("index.html", current_time=datetime.utcnow(),form=form,name=session.get(
+        'name'),known=session.get('known'))
 
 @app.route('/<name>')
 def hello_world(name):
+    session['name'] = name
     return render_template("user.html", name=name)
 
 @app.errorhandler(404)
