614c4cc6ca41d353425edf3883953ad10ebd09cb
openlab <fu_zhipeng@163.com>
2018-08-25 16:47:19 +0800

    user login

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import db
+from flask_login import UserMixin
+from . import login_manager
 
 
 class Role(db.Model):
@@ -13,10 +15,12 @@ class Role(db.Model):
         return '<Role {}>'.format(self.name)
 
 
-class User(db.Model):
+#UserMixin为flask-login插件所必须
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
@@ -33,3 +37,8 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+#load_user函数为flask_login必须
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
