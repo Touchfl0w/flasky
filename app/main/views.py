e92c39e427f995452554638df381682ae3238e70
openlab <fu_zhipeng@163.com>
2018-09-01 13:53:59 +0800

    数据库被替换为MySQL

--- app/main/views.py
+++ app/main/views.py
@@ -9,23 +9,6 @@ from . import main
 
 @main.route('/', methods=['get', 'post'])
 def index():
-    # form = NameForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.name.data).first()
-    #     if not user:
-    #         new_user = User(username=form.name.data)
-    #         db.session.add(new_user)
-    #         db.session.commit()
-    #         # session['known'] = False
-    #         result = send_mail(current_app.config['MAIL_ADMIN'], 'new user registered', 'mail/registry', user=new_user)
-    #         if result:
-    #             flash('邮件已发送！')
-    #         else:
-    #             flash('邮件发送失败！')
-    #     else:
-    #         session['known'] = True
-    #     session['name'] = form.name.data
-    #     return redirect(url_for('main.index'))
     return render_template("index.html", current_time=datetime.utcnow())
 
 
